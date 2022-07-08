147d80f343c1aa9973128ab6077be2e42790d0fc
sivalakshmanna <lakshmansiva07@gmail.com>
2022-07-08 23:24:06 +0530

    add go file

---
+++ build.groovy
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+    
+    stages{
+        stage("chekout code"){
+            steps{
+                println "clone our code to our repository"
+                sh "ls -l"
+                sh "ls -lart ./*"
+                git branch: 'main', url: 'https://github.com/sivalakshmanna/cicd-demo-golang'
+                sh "go build main.go"
+
+            }
+        }
+    }
+        stage("build code"){
+            steps{
+                println "go build"
+                sh "go build main.go"
+                sh "ls -l target/"
+            }
+        }
+}
